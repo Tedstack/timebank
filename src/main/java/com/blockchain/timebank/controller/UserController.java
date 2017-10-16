03b67608ec775c627601a88b1c355f3b3e82a324
toyking <toy_king@163.com>
2017-10-16 12:00:48 +0800

    添加用户接口

--- src/main/java/com/blockchain/timebank/controller/UserController.java
+++ src/main/java/com/blockchain/timebank/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.blockchain.timebank.controller;
 
+import com.blockchain.timebank.entity.UserEntity;
 import com.blockchain.timebank.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
 
 
 @Controller
@@ -17,7 +20,14 @@ public class UserController {
 
     @RequestMapping(value = "login", method = RequestMethod.GET)
     public String userLogin(ModelMap map) {
-        //userService.findUserEntityById(1L);
+        UserEntity userEntity=new UserEntity();
+        userEntity.setName("测试姓名");
+        userEntity.setPhone("测试号码");
+        userEntity.setTimeCoin(0);
+        userEntity.setTimeVol(0);
+        userEntity.setPassword("测试密码");
+        userEntity.setRegisterDate(new java.sql.Date(new Date().getTime()));
+        userService.saveUserEntity(userEntity);
         return "login";
     }
 
