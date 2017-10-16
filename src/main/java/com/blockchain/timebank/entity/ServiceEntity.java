21d5d3dc236351aae42fbf47933d497522da1561
toyking <toy_king@163.com>
2017-10-16 15:48:07 +0800

    添加用户接口

---
+++ src/main/java/com/blockchain/timebank/entity/ServiceEntity.java
@@ -0,0 +1,93 @@
+package com.blockchain.timebank.entity;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "service", schema = "mydb", catalog = "")
+public class ServiceEntity {
+    private long id;
+    private String type;
+    private String name;
+    private double price;
+    private Timestamp updateTime;
+
+    @Id
+    @Column(name = "ID", nullable = false)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "Type", nullable = false, length = 45)
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Basic
+    @Column(name = "Name", nullable = false, length = 45)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "Price", nullable = false, precision = 0)
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Basic
+    @Column(name = "UpdateTime", nullable = true)
+    public Timestamp getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Timestamp updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ServiceEntity that = (ServiceEntity) o;
+
+        if (id != that.id) return false;
+        if (Double.compare(that.price, price) != 0) return false;
+        if (type != null ? !type.equals(that.type) : that.type != null) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (updateTime != null ? !updateTime.equals(that.updateTime) : that.updateTime != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (type != null ? type.hashCode() : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        temp = Double.doubleToLongBits(price);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (updateTime != null ? updateTime.hashCode() : 0);
+        return result;
+    }
+}
--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,5 +1,13 @@
 # User 用户表
-CREATE TABLE `mydb`.`User` ( `ID` BIGINT NOT NULL AUTO_INCREMENT COMMENT '用户编号' , `Name` VARCHAR(20) NULL COMMENT '姓名' , `Password` VARCHAR(45) NULL COMMENT '密码' , `Phone` VARCHAR(20) NOT NULL COMMENT '手机号' , `TimeCoin` DOUBLE NOT NULL COMMENT '时间币余额' , `TimeVol` DOUBLE NOT NULL COMMENT '志愿者时间余额' , `RegisterDate` DATE NULL COMMENT '注册时间' , PRIMARY KEY (`ID`)) ENGINE = InnoDB;
+CREATE TABLE `mydb`.`User` ( `ID` BIGINT NOT NULL AUTO_INCREMENT COMMENT '用户编号' , `Name` VARCHAR(20) NULL COMMENT '姓名' , `Password` VARCHAR(45) NULL COMMENT '密码' , `Phone` VARCHAR(20) NOT NULL COMMENT '手机号' , `TimeCoin` DOUBLE NOT NULL COMMENT '时间币余额' , `TimeVol` DOUBLE NOT NULL COMMENT '志愿者时间余额' , `RegisterDate` DATE NULL COMMENT '注册时间' , PRIMARY KEY (`ID`), UNIQUE (`Phone`) ) ENGINE = InnoDB COMMENT = '用户表';
 
-# TIMEACCOUNT 时间元表
+# Service 服务种类表
+CREATE TABLE `mydb`.`Service` ( `ID` BIGINT NOT NULL AUTO_INCREMENT COMMENT '服务种类编号' , `Type` VARCHAR(45) NOT NULL COMMENT '服务类型' , `Name` VARCHAR(45) NOT NULL COMMENT '服务名称' , `Price` DOUBLE NOT NULL COMMENT '服务的参考价（时间元）' , `UpdateTime` DATETIME NULL COMMENT '价格更新表时间' , PRIMARY KEY (`ID`)) ENGINE = InnoDB COMMENT = '服务种类表';
+
+#TimeAccount 时间元表
+
+
+
+
+
 
