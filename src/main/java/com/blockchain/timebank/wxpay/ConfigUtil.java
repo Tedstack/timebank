767e8659cc635cd14ba7767defca1a187d2a3666
yang <yxllll@qq.com>
2017-12-09 17:52:29 +0800

    1. 增加了微信支付功能相关的后台代码（/com/blockchain/timebank/wxpay） 2. pom文件中添加了支付相关依赖

---
+++ src/main/java/com/blockchain/timebank/wxpay/ConfigUtil.java
@@ -0,0 +1,17 @@
+package com.blockchain.timebank.wxpay;
+
+/**
+ * 服务号相关信息
+ * 支付相关参数
+ */
+
+public class ConfigUtil {
+
+    public final static String APPID = "***";//服务号的应用号
+    public final static String MCH_ID = "***";//商户号
+    public final static String API_KEY = "***";//API密钥
+    public final static String SIGN_TYPE = "MD5";//签名加密方式
+
+
+    public final static String UNIFIED_ORDER_URL = "https://api.mch.weixin.qq.com/pay/unifiedorder";//统一下单地址
+}
---
+++ src/main/java/com/blockchain/timebank/wxpay/WXPayController.java
@@ -0,0 +1,9 @@
+package com.blockchain.timebank.wxpay;
+
+/**
+ * 支付的实现
+ */
+
+public class WXPayController {
+
+}
