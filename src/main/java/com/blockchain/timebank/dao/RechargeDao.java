2c62318520d17259bba2df7b80c3bd09b94303a1
toyking <toy_king@163.com>
2017-12-23 17:59:49 +0800

    1. 完善了支付接口

--- src/main/java/com/blockchain/timebank/dao/RechargeDao.java
+++ src/main/java/com/blockchain/timebank/dao/RechargeDao.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RechargeDao extends CrudRepository<RechargeEntity, Long> {
+    RechargeEntity findByUuid(String uuid);
 }
