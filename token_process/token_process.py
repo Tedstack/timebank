e8cfcfb17c657d3792276a5182225691766e5b2a
Mihaly-Ma <mihaly960308@gmail.com>
2018-01-22 10:25:55 +0800

    1. 加入了一个获取access_token的脚本 服务器上可通过 systemctl status token_process 查看运行情况 log位置在/home/ubuntu/timebank/token_process/log.txt 脚本位置在/home/ubuntu/timebank/token_process/token_process.py

---
+++ token_process/token_process.py
@@ -0,0 +1,92 @@
+import requests
+import json
+import threading
+import time
+from flask import Flask, make_response
+
+app = Flask(__name__)
+
+access_token = ''
+js_api_ticket = ''
+access_token_beta = ''
+js_api_ticket_beta = ''
+
+
+def get_time():
+    return time.strftime("%b %d %Y %H:%M:%S", time.localtime(time.time()))
+
+
+def get_access_token():
+    token_url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET'
+    js_api_url = 'https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=ACCESS_TOKEN&type=jsapi'
+
+    app_id = 'wx1c0cd113cada8f82'
+    app_secret = 'f03286c4132505647a6a5d69cd545bf9'
+
+    global access_token, js_api_ticket
+
+    log = open('/home/ubuntu/timebank/token_process/log.txt', 'w')
+    log.write('start get access token\n\n')
+    while True:
+        while True:
+            try:
+                new_access_token_text = requests.get(token_url.replace('APPID', app_id).replace('APPSECRET', app_secret)).text
+                new_access_token_json = json.loads(new_access_token_text)
+                log.write(get_time() + new_access_token_text + '\n')
+                if new_access_token_json.get('errcode'):
+                    log.write(get_time() + ' error: ' + new_access_token_json.get('errmsg') + '\n')
+                    time.sleep(10)
+                    continue
+                new_access_token = new_access_token_json.get('access_token')
+                if new_access_token:
+                    access_token = new_access_token
+                    log.write(get_time() + 'get access token: ' + access_token + '\n')
+                    break
+                else:
+                    log.write(get_time() + 'error: ' + new_access_token_text + '\n')
+                    time.sleep(10)
+            except Exception as e:
+                log.write(get_time() + e + '\n')
+                continue
+
+        while True:
+            try:
+                new_js_api_ticket_text = requests.get(js_api_url.replace('ACCESS_TOKEN', access_token)).text
+                new_js_api_ticket_json = json.loads(new_js_api_ticket_text)
+                log.write(get_time() + new_js_api_ticket_text + '\n')
+                if new_js_api_ticket_json.get('errcode') != 0:
+                    log.write(get_time() + 'error: ' + new_js_api_ticket_json.get('errmsg') + '\n')
+                    time.sleep(10)
+                    continue
+                new_js_api_ticket = new_js_api_ticket_json.get('ticket')
+                if new_js_api_ticket:
+                    js_api_ticket = new_js_api_ticket
+                    log.write(get_time() + 'get ticket: ' + js_api_ticket + '\n')
+                    break
+                else:
+                    log.write(get_time() + 'error: ' + new_js_api_ticket_text + '\n')
+                    time.sleep(10)
+            except Exception as e:
+                log.write(get_time() + e + '\n')
+                continue
+
+        log.flush()
+        last_hour = int(time.time()) // 3600
+        while int(time.time()) // 3600 == last_hour:
+            time.sleep(60)
+
+
+@app.route('/access_token')
+def return_access_token():
+    return make_response(access_token)
+
+
+@app.route('/js_api_ticket')
+def return_js_api_ticket():
+    return make_response(js_api_ticket)
+
+
+if __name__ == '__main__':
+    token_thread = threading.Thread(target=get_access_token)
+    token_thread.start()
+    app.run(host="0.0.0.0", port=8686, threaded=True)
