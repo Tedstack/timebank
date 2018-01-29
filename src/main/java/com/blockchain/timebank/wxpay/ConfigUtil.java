92c1f31c5c52d7834b558b9a168a4ecbd52c696e
Mihaly-Ma <mihaly960308@gmail.com>
2018-01-29 16:39:20 +0800

    1. 更新测试号配置

--- src/main/java/com/blockchain/timebank/wxpay/ConfigUtil.java
+++ src/main/java/com/blockchain/timebank/wxpay/ConfigUtil.java
@@ -7,9 +7,9 @@ package com.blockchain.timebank.wxpay;
 
 public class ConfigUtil {
 
-    public final static String APPID = "wx1c0cd113cada8f82";//服务号的应用号
+    public final static String APPID = "wxb0f6b07f01978a2a";//服务号的应用号
     public final static String MCH_ID = "1491443062";//商户号
-    public final static String API_KEY = "ae395a815ae2e7a9406cb29e3929e918";//API密钥
+    public final static String API_KEY = "386ef712d87480fa1dc27a93995936eb";//API密钥
     public final static String SIGN_TYPE = "MD5";//签名加密方式
     public final static String NOTIFY_URL = "http://www.i-huzhu.com/timebanktest/recharge/weixin_notify";//异步接收微信支付结果通知的回调地址，通知url必须为外网可访问的url，不能携带参数。
     public final static String TRADE_TYPE_JS = "JSAPI";
