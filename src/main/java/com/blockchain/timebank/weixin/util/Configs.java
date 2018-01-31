7be9744687d660e947562074ea1a55771e76c2eb
Oscar <jiushiwospd@qq.com>
2018-01-31 19:44:49 +0800

    配置域名等

--- src/main/java/com/blockchain/timebank/weixin/util/Configs.java
+++ src/main/java/com/blockchain/timebank/weixin/util/Configs.java
@@ -2,6 +2,15 @@ package com.blockchain.timebank.weixin.util;
 
 public class Configs {
 
+    //106.75.209.252  120.132.30.227
+    //wxb0f6b07f01978a2a  wx1c0cd113cada8f82
+    //386ef712d87480fa1dc27a93995936eb  f03286c4132505647a6a5d69cd545bf9
+    //www.i-huzhu.com  www.i-linli.com
+    /*public static final String IP = "120.132.30.227";
+    public static final String APPID = "wx1c0cd113cada8f82";
+    public static final String APPSECRET = "f03286c4132505647a6a5d69cd545bf9";
+    public static final String DOMAINNAME = "www.i-linli.com";*/
+
     public static final String IP = "106.75.209.252";
     public static final String APPID = "wxb0f6b07f01978a2a";
     public static final String APPSECRET = "386ef712d87480fa1dc27a93995936eb";
