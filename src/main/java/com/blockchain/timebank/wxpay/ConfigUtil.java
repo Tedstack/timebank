dc00ead46a36f84ffd3da4013dbd570db9d5489d
MiaHan <hanyx97@icloud.com>
2018-02-01 11:11:41 +0800

    修改微信支付问题，在修改配置时不要随意修改微信支付的秘钥wxpay.configUtil.java

--- src/main/java/com/blockchain/timebank/wxpay/ConfigUtil.java
+++ src/main/java/com/blockchain/timebank/wxpay/ConfigUtil.java
@@ -11,7 +11,9 @@ public class ConfigUtil {
 
     public final static String APPID = Configs.APPID;//服务号的应用号
     public final static String MCH_ID = "1491443062";//商户号
-    public final static String API_KEY = Configs.APPSECRET;//API密钥
+    //public final static String API_KEY = Configs.APPSECRET;//API密钥
+    //微信支付秘钥和公众号秘钥不同，所以请不要乱改
+    public final static String API_KEY = "ae395a815ae2e7a9406cb29e3929e918"; //API密钥
     public final static String SIGN_TYPE = "MD5";//签名加密方式
     public final static String NOTIFY_URL = "http://"+ Configs.DOMAINNAME+"/timebanktest/recharge/weixin_notify";//异步接收微信支付结果通知的回调地址，通知url必须为外网可访问的url，不能携带参数。
     public final static String TRADE_TYPE_JS = "JSAPI";
