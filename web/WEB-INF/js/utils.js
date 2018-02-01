891d86e6017542a5c6928a18f6db5142af54f629
MiaHan <hanyx97@icloud.com>
2018-02-01 17:22:35 +0800

    增加分享到朋友圈，发送给朋友的方法

--- web/WEB-INF/js/utils.js
+++ web/WEB-INF/js/utils.js
@@ -49,7 +49,8 @@ function get_wx_config () {
                 timestamp : data.timestamp,
                 nonceStr : data.nonceStr,
                 signature : data.signature,
-                jsApiList : [ 'closeWindow' ]
+                jsApiList : [ 'closeWindow', 'onMenuShareTimeline',
+                    'onMenuShareAppMessage' ]
             });
         }
     });
@@ -73,5 +74,42 @@ function back_exit(){
                 }
             }
         }, false);
+    });
+}
+//增加分享链接到微信朋友圈和分享给朋友
+function share(title,link,desc){
+    wx.ready(function(){
+        // wx.hideOptionMenu();
+        wx.onMenuShareTimeline({
+            title: title,
+            link: link,
+            imgUrl: '',
+            success: function () {
+                // 用户确认分享后执行的回调函数
+                alert('分享到朋友圈成功');
+            },
+            cancel: function () {
+                // 用户取消分享后执行的回调函数
+                alert('你没有分享到朋友圈');
+            }
+        });
+        wx.onMenuShareAppMessage({
+            title:title,
+            desc: desc ,
+            link: link ,
+            imgUrl: '',
+            trigger: function (res) {
+                // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
+            },
+            success: function (res) {
+                alert('分享给朋友成功');
+            },
+            cancel: function (res) {
+                alert('你没有分享给朋友');
+            },
+            fail: function (res) {
+                alert(JSON.stringify(res));
+            }
+        });
     });
 }
