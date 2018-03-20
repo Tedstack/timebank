8f8cde4682c0b050e0fa4987ef118aff18e0ff7a
Tedstack <1072265002@qq.com>
2018-03-20 14:26:24 +0800

    test-3

--- web/WEB-INF/js/utils.js
+++ web/WEB-INF/js/utils.js
@@ -80,37 +80,41 @@ function back_exit(){
 function share(){
     wx.ready(function(){
         // wx.hideOptionMenu();
-        wx.onMenuShareTimeline({
-            title: '活动链接测试',
-            link: 'http://www.i-linli.com/timebanktest/team/teamActivityDetails?type=0&activityID=48',
-            imgUrl: 'http://www.i-linli.com/timebanktest/img/activityImg/活动.png',
-            success: function () {
-                // 用户确认分享后执行的回调函数
-                alert('分享到朋友圈成功');
-            },
-            cancel: function () {
-                // 用户取消分享后执行的回调函数
-                alert('你没有分享到朋友圈');
-            }
-        });
-        wx.onMenuShareAppMessage({
-            title:'活动链接测试',
-            desc: '这是一个分享活动链接的测试' ,
-            link: 'http://www.i-linli.com/timebanktest/team/teamActivityDetails?type=0&activityID=48' ,
-            imgUrl: 'http://www.i-linli.com/timebanktest/img/activityImg/活动.png',
-            trigger: function (res) {
-                // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
-            },
-            success: function (res) {
-                alert('分享给朋友成功');
-            },
-            cancel: function (res) {
-                alert('你没有分享给朋友');
-            },
-            fail: function (res) {
-                alert(JSON.stringify(res));
-            }
-        });
+        document.querySelector('#onMenuShareTimeline').onclick = function () {
+            wx.onMenuShareTimeline({
+                title: '活动链接测试',
+                link: 'http://www.i-linli.com/timebanktest/team/teamActivityDetails?type=0&activityID=48',
+                imgUrl: 'http://www.i-linli.com/timebanktest/img/activityImg/活动.png',
+                success: function () {
+                    // 用户确认分享后执行的回调函数
+                    alert('分享到朋友圈成功');
+                },
+                cancel: function () {
+                    // 用户取消分享后执行的回调函数
+                    alert('你没有分享到朋友圈');
+                }
+            });
+        };
+        document.querySelector('#onMenuShareAppMessage').onclick = function () {
+            wx.onMenuShareAppMessage({
+                title: '活动链接测试',
+                desc: '这是一个分享活动链接的测试' ,
+                link: 'http://www.i-linli.com/timebanktest/team/teamActivityDetails?type=0&activityID=48' ,
+                imgUrl: 'http://www.i-linli.com/timebanktest/img/activityImg/活动.png',
+                trigger: function (res) {
+                    // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
+                },
+                success: function (res) {
+                    alert('分享给朋友成功');
+                },
+                cancel: function (res) {
+                    alert('你没有分享给朋友');
+                },
+                fail: function (res) {
+                    alert(JSON.stringify(res));
+                }
+            });
+        };
     });
 }
 function photoCompress(file,w,objDiv){
